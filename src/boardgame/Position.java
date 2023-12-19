38aa9a84be59f9ed1497ac959d322b41c2f3b390
Rafael Cecyn <rafael.mendes112@hotmail.com>
2023-12-19 19:56:20 -0300

    Change in the Class Position

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -5,6 +5,9 @@ public class Position {
 	private Integer row;
 	private Integer column;
 	
+	public Position() {
+	}
+	
 	public Position(Integer row, Integer column) {
 		this.row = row;
 		this.column = column;
